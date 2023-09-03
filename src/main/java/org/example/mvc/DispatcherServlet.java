6ae53d3ccde9bcdefc3a068b420c53d4a9595504
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-04 06:14:12 +0900

    feat: 회원 조회, 등록 컨트롤러 등록
    - HandlerKey로 uriPath 처리 구현
    - redirect 처리 구현
    - user model, repository 간이 구현
    - view, viewResolver 처리 구현

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,16 +1,21 @@
 package org.example.mvc;
 
-import org.example.controller.Controller;
+import org.example.mvc.controller.Controller;
+import org.example.mvc.controller.RequestMethod;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
@@ -19,21 +24,32 @@ public class DispatcherServlet extends HttpServlet {
 
     private RequestMappingHandler requestMappingHandler;
 
+    private List<ViewResolver> viewResolvers;
+
     @Override
-    public void init() throws ServletException {
+    public void init() {
         requestMappingHandler = new RequestMappingHandler();
         requestMappingHandler.init();
+
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.info("[DispatcherServlet] service started");
 
-        Controller handler = requestMappingHandler.findHandler(request.getRequestURI());
+        try {
+            Controller handler = requestMappingHandler.findHandler(new HandlerKey(RequestMethod.valueOf(request.getMethod()), request.getRequestURI()));
+            String viewName = handler.handleRequest(request, response);
 
-        String viewName = handler.handleRequest(request, response);
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(viewName);
+                view.render(request.getParameterMap(), request, response);
+            }
+        } catch (Exception e) {
+            log.error("exception occured: [{}]", e.getMessage() ,e);
+            throw new ServletException(e);
+        }
 
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
-        requestDispatcher.forward(request, response);
     }
 }
--- src/main/java/org/example/mvc/controller/HomeController.java
+++ src/main/java/org/example/mvc/controller/HomeController.java
@@ -1,4 +1,4 @@
-package org.example.controller;
+package org.example.mvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -7,6 +7,6 @@ public class HomeController implements Controller {
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
-        return "home.jsp";
+        return "home";
     }
 }
